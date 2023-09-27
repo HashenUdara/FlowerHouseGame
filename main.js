64eb1d2cd8d4431b4354c98e9f83cf1febe72f96
PasinduAnjana <pasinduanjana000@gmail.com>
2023-09-27 20:30:21 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+import * as THREE from 'three';
+import "./style.css"
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { animate, initScene } from './scene';
+import { importGLB } from './3dObjects';
+
+
+const scene=initScene();
+
+//screen size
+const sizes={
+  width:window.innerWidth,
+  height:window.innerHeight,
+}
+
+
+function main(){
+  
+
+  const renderer = new THREE.WebGLRenderer();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( renderer.domElement );
+
+  const camera=scene.camera;
+
+  const controls = new OrbitControls( camera, renderer.domElement );
+
+
+
+  //resize
+window.addEventListener("resize",()=>{
+  //update size
+  sizes.width=window.innerWidth;
+  sizes.height=window.innerHeight;
+
+  //update camera
+  camera.aspect=sizes.width/sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width,sizes.height)
+
+})
+
+
+  animate(renderer, scene.scene, camera);
+}
+
+main();
+
+
+
